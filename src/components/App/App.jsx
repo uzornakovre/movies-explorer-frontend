6720201e6e5a7946477e60c0265e343f3b632ebb
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-11 23:52:21 +0300

    feat: browser router connected

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="page__content">
-      
+      <Routes>
+        <Route path="/" element={
+          <p>sdfsdfsdfdfsdfsdfsdfsdfsdfsdfsdfsdf</p>
+        } />
+        {/* <Route path="/movies" />
+        <Route path="/saved-movies" />
+        <Route path="/profile" />
+        <Route path="/signin" />
+        <Route path="/signup" /> */}
+      </Routes>
     </div>
   )
 }
