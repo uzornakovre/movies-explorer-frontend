1b4bed468117cbb612b8b4c2e6550f6fc17d530c
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-13 02:50:24 +0300

    feat: footer (desktop)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,27 @@
 function Footer() {
   return (
-    <></>
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__top">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className="footer__bottom">
+          <p className="footer__copyright">&copy; 2020</p>
+          <ul className="footer__links-list">
+            <li className="footer__links-item">
+              <a className="footer__link"
+                href="https://practicum.yandex.ru/"
+                target="_blank"
+                rel="noreferrer">Яндекс.Практикум</a>
+            </li>
+            <li className="footer__links-item">
+              <a className="footer__link"
+                href="https://github.com/uzornakovre"
+                target="_blank"
+                rel="noreferrer">Github</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 }
 
