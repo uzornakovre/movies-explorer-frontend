d9b56283cbd135fea7aff3b759ed08443127e425
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-13 02:26:31 +0300

    feat: portfolio (desktop)

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,16 @@
+import PortfolioLink from "../PortfolioLink/PortfolioLink";
+
 function Portfolio() {
   return (
-    <></>
+    <section className="portfolio">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <PortfolioLink name="Статичный сайт"
+                     link="https://uzornakovre.github.io/how-to-learn/" />
+      <PortfolioLink name="Адаптивный сайт"
+                     link="https://uzornakovre.github.io/russian-travel/" />
+      <PortfolioLink name="Одностраничное приложение"
+                     link="https://mesto-project.website/" />
+    </section>
   )
 }
 
