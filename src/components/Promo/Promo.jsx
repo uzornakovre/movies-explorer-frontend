5711ac8ad0ab2319eae43ff1b419d650d61a480b
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-12 22:15:34 +0300

    feat: promo (desktop)

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,6 +1,17 @@
+import earthLogo from '../../images/earth.png'
+
 function Promo() {
   return (
-    <></>
+    <section className="promo">
+      <div className="promo__content">
+        <div className="promo__heading">
+          <h1 className="promo__heading-title">Учебный проект студента факультета Веб-разработки.</h1>
+          <p className="promo__heading-subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        </div>
+        <img className="promo__image" src={earthLogo} alt="Изображение планеты земля из слов Веб" />
+        <button className="promo__button" type="button">Узнать больше</button>
+      </div>
+    </section>
   )
 }
 
