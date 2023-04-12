a1a206b481d9150274208be748f12d9201017e44
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-13 00:26:22 +0300

    feat: techs (desktop)

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -1,6 +1,24 @@
 function Techs() {
   return (
-    <></>
+    <section className="techs">
+      <div className="techs__content">
+        <h2 className="techs__title">Технологии</h2>
+        <div className="techs__description">
+          <h3 className="techs__subtitle">7 технологий</h3>
+          <p className="techs__text">На курсе веб-разработки мы освоили технологии, 
+          которые применили в дипломном проекте.</p>
+          <ul className="techs__list">
+            <li className="techs__list-item">HTML</li>
+            <li className="techs__list-item">CSS</li>
+            <li className="techs__list-item">JS</li>
+            <li className="techs__list-item">React</li>
+            <li className="techs__list-item">Git</li>
+            <li className="techs__list-item">Express.js</li>
+            <li className="techs__list-item">mongoDB</li>
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
