4a7d0744838fcc742da86d90207b7a84973e60ef
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-18 19:00:43 +0300

    feat: preloader

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,6 +2,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+// import Preloader from '../Preloader/Preloader';
 // import MoviesCard from '../MoviesCard/MoviesCard';
 // import Preloader from '../Preloader/Preloader';
 
--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,7 +1,13 @@
-function Preloader() {
-  return (
-    <></>
-  )
-}
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
+        </div>
+    )
+};
 
 export default Preloader;
