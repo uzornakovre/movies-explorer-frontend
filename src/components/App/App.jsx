6540f92dcb2f3be45e6a278e473f87033bcb06fb
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-18 23:20:54 +0300

    feat: not-found-page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import Profile from '../Profile/Profile';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/profile" element={<Profile />}/>
         <Route path="/signin" element={<Login />}/>
         <Route path="/signup" element={<Register />}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
