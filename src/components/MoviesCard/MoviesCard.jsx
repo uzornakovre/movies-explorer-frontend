49bd27c9c0fdbef804ecd67153591eca74177242
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-18 20:51:16 +0300

    feat: movies-card-list (desktop)

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,6 +1,36 @@
+import testImage from '../../images/test_image.jpg';
+import checkMark from '../../images/check_mark.svg';
+import { useState, useEffect } from 'react';
+
 function MoviesCard() {
+  const [isMovieSaved, setIsMovieSaved] = useState(false);
+  const [buttonContent, setButtonContent] = useState('Сохранить');
+
+  function handleSaveClick() {
+    if (!isMovieSaved) {
+      setIsMovieSaved(true);
+    } else setIsMovieSaved(false);
+  }
+
+  useEffect(() => {
+    if (isMovieSaved) {
+      setButtonContent(<img src={checkMark} alt="Фильм добавлен" />)
+    } else setButtonContent('Сохранить');
+  }, [isMovieSaved, buttonContent]);
+
   return (
-    <></>
+    <article className="movies-card">
+      <div className="movies-card__heading">
+        <h3 className="movies-card__title">В погоне за Бенкси</h3>
+        <p className="movies-card__duration">27 минут</p>
+      </div>
+      <img className="movies-card__image" src={testImage} alt="Здесь будет описание карточки" />
+      <div className="movies-card__caption">
+        <button className={`movies-card__save-button ${isMovieSaved && "movies-card__save-button_active"}`}
+                type="button"
+                onClick={handleSaveClick}>{buttonContent}</button>
+      </div>
+    </article>
   )
 }
 
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,6 +1,30 @@
+import MoviesCard from '../MoviesCard/MoviesCard';
+
 function MoviesCardList() {
+  // const MoviesCardElements = MoviesCards.map(moviesCard => (
+  //   <li key={moviesCard._id}>
+  //     <MoviesCard card={moviesCard} />
+  //   </li>
+  // ));
+
   return (
-    <></>
+    <section className="movies" aria-label="Фильмы">
+      <ul className="movies__list">
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+        <li><MoviesCard /></li>
+      </ul>
+      <button className="movies__load-more" type="button">Ещё</button>
+    </section>
   )
 }
 
