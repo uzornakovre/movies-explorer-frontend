0e3d105d6900c9f46ea1b8db41d99ba7fec2955f
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-04-20 02:01:41 +0300

    profile page (mobile) + navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,30 @@
 import Burger from "../BurgerMenu/Burger";
+import { Link } from 'react-router-dom';
 
 function Navigation({ page, type, place }) {
+
   return (
     <nav className={`navigation navigation_type_${type}`}>
       {type === "logged-in" && 
         <>
           <ul className={`navigation__menu navigation__menu_type_movies navigation__menu_place_${place}`}>
             <li className="navigation__menu-item">
-              <button className={`navigation__button navigation__button_${page === "movies" && "active"}`} 
-                      type="button">Фильмы</button>
+              { <Link to="/movies" className={`navigation__button navigation__button_${page === "movies" && "active"}`}>
+                Фильмы
+                </Link> }
             </li>
             <li className="navigation__menu-item">
-              <button className={`navigation__button navigation__button_${page === "saved-movies" && "active"}`} 
-                      type="button">Сохранённые фильмы</button>
+              { <Link to="/saved-movies" className={`navigation__button navigation__button_${page === "saved-movies" && "active"}`}>
+                  Сохранённые фильмы
+                </Link> }
             </li>
           </ul>
           <ul className={`navigation__menu navigation__menu_type_account navigation__menu_place_${place}`}>
             <li className="navigation__menu-item">
-              <button className={`navigation__button navigation__button_type_account`} 
-                      type="button">
-                <p className="navigation__button-text">Аккаунт</p>
-                <div className="navigation__account-logo"></div>
-              </button>
+              { <Link to="/profile" className={`navigation__button navigation__button_type_account`}>
+                  <p className="navigation__button-text">Аккаунт</p>
+                  <div className="navigation__account-logo"></div>
+                </Link> }
             </li>
           </ul>
           <Burger />
@@ -30,12 +33,14 @@ function Navigation({ page, type, place }) {
       {type === "start-page" &&
         <ul className="navigation__menu navigation__menu_type_account">
           <li className="navigation__menu-item">
-            <button className={`navigation__button navigation__button_${page === "" && "active"}`} 
-                    type="button">Регистрация</button>
+            { <Link to="/signup" className={`navigation__button navigation__button_${page === "" && "active"}`}>
+              Регистрация
+              </Link> }
           </li>
           <li className="navigation__menu-item">
-            <button className={`navigation__button navigation__button_${page === "" && "active"} navigation__button_type_login`} 
-                    type="button">Войти</button>
+            { <Link to="/signin" className={`navigation__button navigation__button_${page === "" && "active"} navigation__button_type_login`}>
+                Войти
+              </Link> }
           </li>
         </ul>
       }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,14 +11,16 @@ function Profile() {
       <div className="profile">
         <h2 className="profile__title">{`Привет, ${userName}!`}</h2>
         <table className="profile__info">
-          <tr className="profile__info-row">
-            <th className="profile__info-heading">Имя</th>
-            <td className="profile__info-data">{userName}</td>
-          </tr>
-          <tr className="profile__info-row">
-            <th className="profile__info-heading">Email</th>
-            <td className="profile__info-data">{userEmail}</td>
-          </tr>
+          <tbody>
+            <tr className="profile__info-row">
+              <th className="profile__info-heading">Имя</th>
+              <td className="profile__info-data">{userName}</td>
+            </tr>
+            <tr className="profile__info-row">
+              <th className="profile__info-heading">E-mail</th>
+              <td className="profile__info-data">{userEmail}</td>
+            </tr>
+          </tbody>
         </table>
         <div className="profile__actions">
           <button className="profile__button profile__button_type_edit" type="button">Редактировать</button>
